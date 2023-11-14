5afcabb6b937a515930f4802fe9915752d062806
ameer017 <rajiabdullahi907@gmail.com>
2023-11-14 09:31:13 +0100

    client side updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import Register from './Register';
-import Login from './Login';
+import Register from "./Register";
+import Login from "./Login";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Admin from "./components/Admin";
+import Editor from "./components/Editor";
+import LinkPage from "./components/LinkPage";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
+import RequireAuth from "./components/RequireAuth";
+import Unauthorized from "./components/Unauthorized";
+
+const ROLES = {
+  'User': 2001,
+  'Editor': 1984,
+  'Admin': 5150
+}
 
 function App() {
-
   return (
-    <main className="App">
-      <Login />
-    </main>
+    // step one -- reroute
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* we want to protect these routes */}
+        {/* leave unprotected at first */}
+        {/* step 2 - create a folder named 'hooks' and file 'useAuth' .. this is to handle the global auth state to avoid rendering conditional statement in the route app component as it is recommended by react-router-dom */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+          <Route path="editor" element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin" element={<Admin />} />
+        </Route>
+
+        <Route
+          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+        >
+          <Route path="lounge" element={<Lounge />} />
+        </Route>
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
